de7a423f6ccf7dc98b77f69ecd2f11662c853b92
suhail <suhail.r@hubspire.com>
2024-09-09 21:12:48 +0530

    header and layout and router-dom implemented

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./global.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRouter from "./AppRouter.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <AppRouter />
+    </Router>
+  </StrictMode>
+);
