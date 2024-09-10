64fc77bbcc0c28f7e4dd79cca5029d77f84d9857
suhail <suhail.r@hubspire.com>
2024-09-10 22:07:44 +0530

    assets  added, login button added, cover image added

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -7,6 +8,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
     </div>
   );
