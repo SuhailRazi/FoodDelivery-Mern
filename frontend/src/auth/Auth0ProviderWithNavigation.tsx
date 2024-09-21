06d936a74049064d11ca126576af1fb56aca52ea
suhail <suhail.r@hubspire.com>
2024-09-22 02:30:06 +0530

    react query implemented, create user hook added

--- frontend/src/auth/Auth0ProviderWithNavigation.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigation.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 type Auth0ProviderWithNavigationProps = {
@@ -7,6 +8,7 @@ type Auth0ProviderWithNavigationProps = {
 const Auth0ProviderWithNavigation: React.FC<
   Auth0ProviderWithNavigationProps
 > = ({ children }) => {
+  const { createUser } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -16,7 +18,12 @@ const Auth0ProviderWithNavigation: React.FC<
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log(user);
+    if (user?.sub && user?.email) {
+      createUser({
+        auth0Id: user?.sub,
+        email: user?.email,
+      });
+    }
   };
 
   return (
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,24 @@ import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./AppRouter.tsx";
 import Auth0ProviderWithNavigation from "./auth/Auth0ProviderWithNavigation.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <Auth0ProviderWithNavigation>
-        <AppRouter />
-      </Auth0ProviderWithNavigation>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigation>
+          <AppRouter />
+        </Auth0ProviderWithNavigation>
+      </QueryClientProvider>
     </Router>
   </StrictMode>
 );
