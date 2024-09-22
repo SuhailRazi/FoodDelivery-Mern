0f4c23060af09cac16c5fe4422e2c4bab9a2132a
suhail <suhail.r@hubspire.com>
2024-09-22 22:24:48 +0530

    create user done in frontend

--- frontend/src/auth/Auth0ProviderWithNavigation.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigation.tsx
@@ -1,5 +1,5 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Auth0ProviderWithNavigationProps = {
   children: React.ReactNode;
@@ -8,7 +8,7 @@ type Auth0ProviderWithNavigationProps = {
 const Auth0ProviderWithNavigation: React.FC<
   Auth0ProviderWithNavigationProps
 > = ({ children }) => {
-  const { createUser } = useCreateMyUser();
+  const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -17,13 +17,8 @@ const Auth0ProviderWithNavigation: React.FC<
     throw new Error("Unable to initialise auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    if (user?.sub && user?.email) {
-      createUser({
-        auth0Id: user?.sub,
-        email: user?.email,
-      });
-    }
+  const onRedirectCallback = () => {
+    navigate("/auth-callback");
   };
 
   return (
