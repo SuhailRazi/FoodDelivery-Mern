499a17381416613178b3ee5c7b35074efc8b593f
suhail <suhail.r@hubspire.com>
2024-09-26 21:23:52 +0530

    token validation middleware, create user api auth porivder

--- frontend/src/auth/Auth0ProviderWithNavigation.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigation.tsx
@@ -12,8 +12,8 @@ const Auth0ProviderWithNavigation: React.FC<
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-
-  if (!domain || !clientId || !redirectUri) {
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("Unable to initialise auth");
   }
 
@@ -25,7 +25,7 @@ const Auth0ProviderWithNavigation: React.FC<
     <Auth0Provider
       clientId={clientId}
       domain={domain}
-      authorizationParams={{ redirect_uri: redirectUri }}
+      authorizationParams={{ redirect_uri: redirectUri, audience }}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
