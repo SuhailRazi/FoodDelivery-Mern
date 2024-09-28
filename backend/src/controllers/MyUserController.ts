b3623f4ec809b7b3465816c1c8082b4c196e7f10
suhail <suhail.r@hubspire.com>
2024-09-28 12:30:46 +0530

    update user, form validator using express-validator, addition parameters assiginig in request

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -14,10 +14,33 @@ const createCurrentUser = async (req: Request, res: Response) => {
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Error crating user" });
+    res.status(500).json({ message: "Error creating user" });
+  }
+};
+
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.country = country;
+    user.city = city;
+
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error udpating user" });
   }
 };
 
 export default {
   createCurrentUser,
+  updateCurrentUser,
 };
