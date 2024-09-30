764846cbe34d4afa877b0696e4ca2c1d225cace5
suhail <suhail.r@hubspire.com>
2024-09-30 22:26:06 +0530

    sonner in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./AppRouter.tsx";
 import Auth0ProviderWithNavigation from "./auth/Auth0ProviderWithNavigation.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,6 +21,12 @@ createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <Auth0ProviderWithNavigation>
           <AppRouter />
+          <Toaster
+            visibleToasts={1}
+            position="top-right"
+            richColors
+            closeButton
+          />
         </Auth0ProviderWithNavigation>
       </QueryClientProvider>
     </Router>
