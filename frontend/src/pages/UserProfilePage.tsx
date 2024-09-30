e6b0d1d6923d2c0e560669ae88ac42cb5f9ef46f
suhail <suhail.r@hubspire.com>
2024-09-30 22:26:18 +0530

    user profile api and page

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,7 +1,10 @@
+import { useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
-  return <UserProfileForm />;
+  const { UpdateUser, isLoading } = useUpdateMyUser();
+
+  return <UserProfileForm onSave={UpdateUser} isLoading={isLoading} />;
 };
 
 export default UserProfilePage;
